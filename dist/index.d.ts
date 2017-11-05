699945b4e5e51963c24066a1ca9ba766992bf144
Resi Respati <resir014@gmail.com>
2017-11-05 21:46:08 +0700

    remove ScreepsInscribe interface

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,41 +1,3 @@
-export interface ScreepsInscribe {
-    /**
-     * Decorates a string of text with color.
-     *
-     * @param {string} str The string to format.
-     * @param {string} fontColor Any HTML color name (`teal`) or hex code (`#33b5e5`).
-     * @returns {string} A string output of the color-formatted text.
-     * @memberof IInscribe
-     */
-    color(str: string, fontColor: string): string;
-    /**
-     * Appends a link to log output
-     *
-     * @param {string} href Any string-escaped link.
-     * @param {string} title The link title.
-     * @returns {string} A console output string with link.
-     * @memberof IInscribe
-     */
-    link(href: string, title: string): string;
-    /**
-     * Allows tooltip to be sent to the formatter
-     *
-     * @param {string} str The string to format
-     * @param {string} tooltipText The tooltip text to give away
-     * @returns {string} The tooltipped text for the Screeps console.
-     * @memberof IInscribe
-     */
-    tooltip(str: string, tooltipText: string): string;
-    /**
-     * Outputs a formatted version of `Game.time`
-     *
-     * @param {string} [color] Any HTML color name (`teal`) or hex code
-     *   (`#33b5e5`). Defaults to `gray` if empty.
-     * @returns {string} A pretty-formatted `Game.time`.
-     * @memberof Inscribe
-     */
-    time(color?: string): string;
-}
 /**
  * Decorates a string of text with color.
  *
@@ -77,4 +39,9 @@ export declare function time(fontColor?: string): string;
  * @export
  * @returns {ScreepsInscribe}
  */
-export default function init(): ScreepsInscribe;
+export default function init(): {
+    color: (str: string, fontColor: string) => string;
+    link: (href: string, title: string) => string;
+    tooltip: (str: string, tooltipText: string) => string;
+    time: (fontColor?: string) => string;
+};
